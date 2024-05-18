e4dff64a5f9d95051f9331b9a27636e5b3457b93
Reaking1 <reabetsweramoswane@gmail.com>
2024-05-18 21:50:34 +0200

    coded first part

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -1,32 +1,47 @@
-import logo from "./assets/Reabetswe1.png"
-import './App.css'
-
+import './App.css';
+import { useEffect } from 'react';
+import { gsap } from 'gsap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faInstagram, faTiktok, faTwitch, faYoutube } from '@fortawesome/free-brands-svg-icons';
+ 
 function App() {
-
+  useEffect(() => {
+    // GSAP animation for the background
+    gsap.to(".section_1", {
+      backgroundPosition: "200% center",
+      duration: 5,
+      repeat: -1,
+      ease: "linear"
+    });
+  }, []);
 
   return (
     <>
       <div className="section_1">
-        <div className="image_logo">
-          <img src={logo} alt="My logo" /> 
+        <div className="box">
+          <img src="path-to-your-image.png" alt="Profile" className="profile-image" />
+          <h1>Developer/Streamer</h1>
         </div>
-        <div className="link_bar one">
-          <h1 className="twicth">Twicth</h1>
+         {/* Add Scrap paper bars */}
+        <div className="bars">
+          <div className="bar">
+            <FontAwesomeIcon icon={faTwitch} beatFade/>
+          </div>
+          <div className="bar">
+          <FontAwesomeIcon icon={faInstagram} beatFade/>
+          </div>
+          <div className="bar">
+          <FontAwesomeIcon icon={faYoutube} beatFade/>
+          </div>
+           {/* Add links to the icons */}
+          <div className="bar">
+          <FontAwesomeIcon icon={faTiktok} beatFade/>
+          </div>
+          {/* Add more bars with icons as needed */}
         </div>
-        <div className="link_bar two">
-          <h1 className="Youtube">Youtube</h1>
-          </div>
-        <div className="link_bar three">
-          <h1 className="tiktok">TikTok</h1>
-          </div>
-          <div className="link_bar four">
-            <h1 className="twitter">Twitter</h1>
-          </div>
-
       </div>
-   
     </>
-  )
+  );
 }
 
-export default App
+export default App;
