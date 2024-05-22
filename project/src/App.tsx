9e1108687226c58e0ca1b21ba9ab4d3c6075bbbf
Reaking1 <reabetsweramoswane@gmail.com>
2024-05-23 01:16:25 +0200

    updated a lot of things

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -3,8 +3,10 @@ import { useEffect } from 'react';
 import { gsap } from 'gsap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faInstagram, faTiktok, faTwitch, faYoutube } from '@fortawesome/free-brands-svg-icons';
+import { faGamepad, faCode, faKeyboard, faLaptopCode, faMobileAlt } from '@fortawesome/free-solid-svg-icons';
 import AnimatedText from './Animated fonts/AnimatedFonts';
- 
+import Logo from './assets/Logo.jpeg'
+
 function App() {
   useEffect(() => {
     // GSAP animation for the background
@@ -14,34 +16,56 @@ function App() {
       repeat: -1,
       ease: "linear"
     });
+
+    // Animate icons
+    const icons = document.querySelectorAll('.background-icon');
+    icons.forEach(icon => {
+      gsap.to(icon, {
+        y: 'random(-20, 20)',
+        x: 'random(-20, 20)',
+        rotation: 'random(-30, 30)',
+        duration: 5,
+        repeat: -1,
+        yoyo: true,
+        ease: 'power1.inOut'
+      });
+    });
   }, []);
 
   return (
     <>
       <div className="section_1">
         <div className="box">
-          <img src="path-to-your-image.png" alt="Profile" className="profile-image" />
+          <img src={Logo} alt="Profile" className="profile-image" />
           <AnimatedText />
         </div>
         <div className="other_bar">
-            
-            </div>
-         {/* Add Scrap paper bars */}
+          <div className="rectangular-bar">Bar 1</div>
+          <div className="rectangular-bar">Bar 2</div>
+          <div className="rectangular-bar">Bar 3</div>
+          <div className="rectangular-bar">Bar 4</div>
+        </div>
         <div className="bars">
           <div className="bar">
             <FontAwesomeIcon icon={faTwitch} beat/>
           </div>
           <div className="bar">
-          <FontAwesomeIcon icon={faInstagram} beat/>
+            <FontAwesomeIcon icon={faInstagram} beat/>
           </div>
           <div className="bar">
-          <FontAwesomeIcon icon={faYoutube} beat/>
+            <FontAwesomeIcon icon={faYoutube} beat/>
           </div>
-           {/* Add links to the icons */}
           <div className="bar">
-          <FontAwesomeIcon icon={faTiktok} beat/>
+            <FontAwesomeIcon icon={faTiktok} beat/>
           </div>
-          {/* Add more bars with icons as needed */}
+        </div>
+        <div className="background-icons">
+          <FontAwesomeIcon icon={faGamepad} className="background-icon" />
+          <FontAwesomeIcon icon={faCode} className="background-icon" />
+          <FontAwesomeIcon icon={faKeyboard} className="background-icon" />
+          <FontAwesomeIcon icon={faLaptopCode} className="background-icon" />
+          <FontAwesomeIcon icon={faMobileAlt} className="background-icon" />
+          {/* Add more icons as needed */}
         </div>
       </div>
     </>
@@ -49,3 +73,4 @@ function App() {
 }
 
 export default App;
+
