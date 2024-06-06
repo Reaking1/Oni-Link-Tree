4ef15614879c6a22fc4de8660f5bf1a2c6e8146e
Reaking1 <reabetsweramoswane@gmail.com>
2024-06-06 21:44:09 +0100

    error 1 done

--- project/src/_tests_/Menu.test.tsx
+++ project/src/_tests_/Menu.test.tsx
@@ -1,8 +1,7 @@
-import React from 'react';
 import { render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
+import Menu from '../Menu/menu';
 
-import Menu from '../Menu';
 
 test('renders menu component', () => {
   const { getByText } = render(<Menu/>);
