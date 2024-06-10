1f21963f3279b8ed523a15282d776b00de752bb5
Reaking1 <reabetsweramoswane@gmail.com>
2024-06-10 19:45:07 -0200

    chnages

--- project/jest.config.cjs
+++ project/jest.config.cjs
@@ -5,5 +5,12 @@ module.exports = {
   moduleNameMapper: {
     '\\.(css|less)$': 'identity-obj-proxy',
     '^@/(.*)$': '<rootDir>/src/$1',
+    '\\.(jpg|jpeg|png|gif|svg)$': '<rootDir>/src/__mocks__/fileMock.js',
   },
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  transformIgnorePatterns: [
+    '/node_modules/(?!(@testing-library|@fortawesome)/)',
+  ],
 };
--- project/src/_tests_/Menu.test.tsx
+++ project/src/_tests_/Menu.test.tsx
@@ -1,9 +1,29 @@
-import { render } from '@testing-library/react';
+// src/_tests_/Menu.test.tsx
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 import Menu from '../Menu/menu';
+import { BrowserRouter as Router } from 'react-router-dom';
 
+test('renders menu component with links', () => {
+  render(
+    <Router>
+      <Menu />
+    </Router>
+  );
 
-test('renders menu component', () => {
-  const { getByText } = render(<Menu/>);
-  const menuElement = getByText(/menu item/i);
-  expect(menuElement).toBeInTheDocument();
+  // Check if specific links are in the document
+  const homeLink = screen.getByRole('link', { name: /home/i });
+  const aboutLink = screen.getByRole('link', { name: /about/i });
+  const contactLink = screen.getByRole('link', { name: /contact/i });
+
+  // Ensure the links are in the document
+  expect(homeLink).toBeInTheDocument();
+  expect(aboutLink).toBeInTheDocument();
+  expect(contactLink).toBeInTheDocument();
+
+  // Optionally, check the href values of the links
+  expect(homeLink).toHaveAttribute('href', '/home');
+  expect(aboutLink).toHaveAttribute('href', '/about');
+  expect(contactLink).toHaveAttribute('href', '/contact');
 });
